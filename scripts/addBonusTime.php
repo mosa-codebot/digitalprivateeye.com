712635eb58a79ec054a6f5c592097ca76b28f4fc
Mosa <mosa.adekunle@gmail.com>
2014-08-11 03:00:07 -0600

    Working on fb bonus time

--- scripts/addBonusTime.php
+++ scripts/addBonusTime.php
@@ -1,16 +1,10 @@
 <?php
-
-include('Mailer.php');
+include('../DPEyeMailer/mailer.php');
 include('dao.php');
 
 $dao = new dao();
 $usersToCredit = $dao->getUsersToCreditBonus();
-
 $mailer = new Mailer();
-
-$fromEmail = "administrator@digitalprivateeye.com";
-$fromName = "Digital Private Eye";
-
 $appName = "Digital Private Eye";
 $websiteUrl = "http://digitalprivateeye.com";
 $viewDevicesUrl = $websiteUrl."/devices.php";
@@ -31,7 +25,6 @@ foreach($usersToCredit as $user)
     $userId = $user['user_id'];
     $email =  $user['username'];
     $daysToAdd =  $user['days_to_add'];
-
     $time = time();
     $creation_date = $time . "111";
     $expiry_date = ($time+64800)."111";
@@ -41,23 +34,23 @@ foreach($usersToCredit as $user)
     $subject = "Bonus Subscription - New coupon activated";
     $body = "Hello,
 
-A bonus coupon (coupon id: $couponCode) has been created for you. You can view all your coupons at $viewCouponsUrl.
+    A bonus coupon (coupon id: $couponCode) has been created for you. You can view all your coupons at $viewCouponsUrl.
+    You can apply your coupon to any device you wish and this will add a subscription period to your device.
+    On the page $viewCouponsUrl , scroll down to the coupon code you wish to use. Select the device you wish to apply it to on your list of devices, and press apply. Done!
+    If you received this email in error or you want your data purged off our systems, follow the link below
+    $websiteUrl/cancel-registration.php?user=$email&uid=$userId
 
-You can apply your coupon to any device you wish and this will add a subscription period to your device.
+    Best wishes from,
+    $websiteUrl
+    $appName ";    
 
-On the page $viewCouponsUrl , scroll down to the coupon code you wish to use. Select the device you wish to apply it to on your list of devices, and press apply. Done!
-
-
-If you received this email in error or you want your data purged off our systems, follow the link below
-$websiteUrl/cancel-registration.php?user=$email&uid=$userId
-
-
-Best wishes from,
-$websiteUrl
-$appName ";
-
-	$dao->addPaymentCoupon($userId, $couponCode, $creation_date, $expiry_date, $used, $paymentId, $daysToAdd);
-        $mailer->SendMail($subject, $body, $fromEmail, $fromName, $email);
-	$dao->MarkBonusCouponCreated($entryId);
+    $mailer->fromEmail = "support@digitalprivateeye.com";
+    $mailer->fromName = "Digital Private Eye";
+    $mailer->replyTo = "support@digitalprivateeye.com";
+    $mailer->to = $email;
+    $mailer->subject = $subject;
+    $mailer->message = $body;
+    $mailer->SendMail();
+    $dao->addPaymentCoupon($userId, $couponCode, $creation_date, $expiry_date, $used, $paymentId, $daysToAdd);
+    $dao->MarkBonusCouponCreated($entryId);
 }
-
